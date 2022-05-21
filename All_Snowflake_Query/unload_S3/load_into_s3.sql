54ba86b3d09ca55466c66ada5dfa607ef0e8c27c
Dhruv <101560010+Pokhariyal@users.noreply.github.com>
2022-05-21 18:04:23 +0530

    Update load_into_s3.sql

--- All_Snowflake_Query/unload_S3/load_into_s3.sql
+++ All_Snowflake_Query/unload_S3/load_into_s3.sql
@@ -6,5 +6,6 @@ COPY INTO @adp/adp/SAFIO_SHIPMENTS/SAFIO_SHIPMENTS.csv from (select distinct(lin
                                                              orderNumber,locationCode,customerId,shipCity,
                                                              shipState,shipCountry,shipPostalCode,accountSkuNumber
                                                                 from SAFIO_SHIPMENTS)
-file_format = (type ='csv' field_optionally_enclosed_by='"' compression=none)  header =true single = True max_file_size = 4900000000;
-
+file_format = (type ='csv' field_optionally_enclosed_by='"' compression=none NULL_IF=())  header =true single = True max_file_size = 4900000000;
+-- ################# challenges
+-- export to deal with null values as showing as /N i used null_if =() in the satement.
