05215c02574f99659c949ea303aad335397fee27
Dhruv <101560010+Pokhariyal@users.noreply.github.com>
2022-05-24 23:16:50 +0530

    Update SAFIO_VENDOR_PRODUCTS.sql

--- All_Snowflake_Query/safio/SAFIO_VENDOR_PRODUCTS.sql
+++ All_Snowflake_Query/safio/SAFIO_VENDOR_PRODUCTS.sql
@@ -6,7 +6,6 @@ create table SAFIO_VENDOR_PRODUCTS (SKU VARCHAR(36),VENDORSKU VARCHAR(49),
                                     purchaseLeadTime varchar default null,VENDORCODE VARCHAR(12),vendorName varchar default null,
                                     fileUpdate TIMESTAMP_NTZ(9) default null
                                    );
-
 INSERT INTO SAFIO_VENDOR_PRODUCTS (SKU,VENDORSKU,CONTRACTEDPRICE,vendorCode)
 with cte as (
 SELECT b.partnumber || '-' || b.cond as SKU,b.acctno || '-' || b.partnumber || '-' || b.cond as vendorSku,
@@ -17,3 +16,7 @@ ON a.DOC_NO = b.DOC_NO
 WHERE b.QORDER >0
 ORDER BY a.ENTER_DATE)
 select SKU,vendorSku,contractedPrice,vendorCode from cte ;
+
+
+COPY INTO @adp/adp/SAFIO_VENDOR_PRODUCTS/vendor_products.csv from (select * from SAFIO_VENDOR_PRODUCTS)
+file_format = (type ='csv' compression=none field_optionally_enclosed_by='"' compression=none NULL_IF=())  header =true single = True max_file_size = 4900000000;
