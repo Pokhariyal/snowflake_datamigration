76fbe3b21385de2b7ea9773d1ccdb625d968004a
Dhruv <101560010+Pokhariyal@users.noreply.github.com>
2022-05-26 17:49:21 +0530

    Update AVSight_Accounts.sql

--- All_Snowflake_Query/safio/AVSight_Accounts.sql
+++ All_Snowflake_Query/safio/AVSight_Accounts.sql
@@ -1,3 +1,19 @@
+create or replace table avsight_accounts (Billing_City varchar(30),Billing_Country varchar(40),Billing_Postal_Code varchar(12),
+                                           Billing_State varchar(4),Billing_Street varchar(81),Description varchar default null, 
+                                FAX varchar(40),name varchar(60),Phone varchar(40),Account_Owner varchar default null ,
+                                Account_Owner_convid varchar default null,
+                                Account_Owner_extid varchar default null,shipping_city varchar(30),shipping_country varchar(40),
+                                Shipping_Postal_Code varchar(12),Shipping_State varchar(4),Shipping_Street varchar(81),
+                                Account_Number varchar(12),Account_Terms varchar default null, Account_Terms_convid varchar default null,
+                                Account_Terms_extid varchar default null,Accounting_Type varchar(1),Active_Billing_Contact varchar default null,
+                                Billing_Contact_convid varchar default null,Billing_Contact_extid varchar default null,Credit_Limit int,
+                                Customer_Priority varchar,EMAIL_ADDRESS varchar, INVOICE_EMAIL varchar default null,Preferred_Ship_Method varchar,
+                                Preferred_Ship_Method_convid varchar default null,Preferred_Ship_Method_extid varchar default null,
+                                Remit_City VARCHAR(30),Remit_Postal_Code VARCHAR(12),Remit_State VARCHAR(4),Remit_Country VARCHAR(40),
+                                Remit_Street VARCHAR(101),Vendor_Terms varchar default null,
+                                Vendor_Terms_convid varchar default null, Vendor_Terms_extid varchar default null,Vat varchar default null,
+                                Tracking1099 varchar default null,External_Reference_ID VARCHAR(12));
+--############################################################
 INSERT INTO avsight_accounts (Billing_City,Billing_Country,Billing_Postal_Code,Billing_State,Billing_Street,
                               FAX,name,Phone,shipping_city,shipping_country,Shipping_Postal_Code,Shipping_State,Shipping_Street,
                               Account_Number,Account_Terms_extid,Accounting_Type,Credit_Limit,Customer_Priority,EMAIL_ADDRESS,
@@ -30,6 +46,10 @@ base.Account_Number,base.Account_Terms_extid,base.Accounting_Type,base.Credit_Li
 base.Preferred_Ship_Method,base.Remit_City,base.Remit_Postal_Code,base.Remit_State,base.Remit_Country,base.Remit_Street,base.acctno as External_Reference_ID  from cte
 join base
 on cte.acctno=base.acctno and cte.subc=base.subc);
+--#####################
+COPY INTO @adp/adp/AVSIGHT_ACCOUNTS/avsight_accounts.csv from AVSIGHT_ACCOUNTS
+file_format = (type ='csv' compression=none field_optionally_enclosed_by='"' compression=none NULL_IF=())  header =true HEADER = TRUE single = true max_file_size = 4900000000;
+
 
 --#####################################################################################################################################################################
 create table AVSight_Accounts as
